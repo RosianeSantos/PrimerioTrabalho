d0b99901162fd3a9e8c5003ba9ac72549c4d9094
Rosy <Rosy@192.168.230.176>
2013-05-17 22:27:07 -0300

    Inserindo BD

--- src/br/edu/ifnmg/DadosClasses/Produto.java
+++ src/br/edu/ifnmg/DadosClasses/Produto.java
@@ -12,19 +12,27 @@ public class Produto {
     
     int idProduto;
     String nomeProduto;
+    String descricao;
     double valorUnitario;
+    double valorVenda;
     double estoque;
     
     public Produto(){
         this.idProduto = 0;
         this.nomeProduto = "";
+        this.descricao = "";
         this.valorUnitario = 0;
+        this.valorVenda = 0;
+        this.estoque = 0;
     }
     
-    public Produto(int id_, String nom, double val){
+    public Produto(int id_, String nom, String desc, double val,double vend, double est){
         this.idProduto = id_;
         this.nomeProduto = nom;
+        this.descricao = desc;
         this.valorUnitario = val;
+        this.valorVenda = vend;
+        this.estoque = est;
     }
 
     public int getIdProduto() {
@@ -43,6 +51,12 @@ public class Produto {
         this.nomeProduto = nomeProduto;
     }
 
+    public String getDescricao(){
+       return descricao;
+    }
+    public void setDescricao(String descricao){
+         this.descricao = descricao;
+    }
     public double getValorUnitario() {
         return valorUnitario;
     }
@@ -51,6 +65,20 @@ public class Produto {
         this.valorUnitario = valorUnitario;
     }
     
+    public double getValorVenda(){
+        return valorVenda;
+    }
     
+    public void setValorVenda(double valorVenda){
+        this.valorVenda = valorVenda;
+    }
     
+    public double getEstoque(){
+        return estoque;
+    }
+    
+    public void setEstoque(double estoque){
+        this.estoque = estoque;
+    }
+     
 }
