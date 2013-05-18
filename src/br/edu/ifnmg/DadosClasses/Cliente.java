381a853d1071b92474365fee7da8a11c5f9c1b10
Rosy <Rosy@192.168.230.176>
2013-05-17 22:26:40 -0300

    Inserindo BD

--- src/br/edu/ifnmg/DadosClasses/Cliente.java
+++ src/br/edu/ifnmg/DadosClasses/Cliente.java
@@ -4,34 +4,95 @@
  */
 package br.edu.ifnmg.DadosClasses;
 
+import java.util.Objects;
+
 /**
  *
  * @author rosy
  */
-public class Cliente extends Pessoa {
-    private int Id;
-    
-    //----
-    
+public class Cliente{
+    int codCliente;
+    String nome;
+    int cpf;
+    int rg;
+    int telefone;
+    String email;
+
+    public int getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    String endereco;
+  
+    /**
+     *
+     */
     public Cliente(){
-        Id = 0;
+        codCliente = 0;
+        nome = "";
+        cpf = 0;
+        rg = 0;
+        email = "";
+        telefone = 0;
+        endereco = "";
+        
+        
     }
+
    
-    //----
-    
-    @Override
-    public int getId(){
-        return Id;
-    }
-    
-    @Override
-    public void setId(int Id){
-        this.Id = Id;
+
+    public String getNome() {
+        return nome;
     }
 
     @Override
     public int hashCode() {
         int hash = 7;
+        hash = 97 * hash + this.codCliente;
+        hash = 97 * hash + Objects.hashCode(this.nome);
         return hash;
     }
 
@@ -44,26 +105,36 @@ public class Cliente extends Pessoa {
             return false;
         }
         final Cliente other = (Cliente) obj;
-        if (this.Id != other.Id) {
+        if (this.codCliente != other.codCliente) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
             return false;
         }
         return true;
     }
 
+    public void setNome(String nom) {
+        if (nom.length()>3 && nom.length()<250){
+            nome = nom;
+        }
+    }
+
     @Override
     public String toString() {
-        return "Cliente{" + "Id=" + Id + '}';
+        return "Cliente{" + "codigo=" + codCliente + ", nome=" + nome + '}';
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    public void setRg(double parseDouble) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void setCpf(double parseDouble) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void setTelefone(double parseDouble) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
+    
